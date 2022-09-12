96e94846257534bee9e297873346224a8a79d435
prasad <prasad@prasad>
2022-09-12 20:16:25 +0530

    commit and push by user1

---
+++ src/test/java/dDT/ContactTest.java
@@ -0,0 +1,22 @@
+package dDT;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createcontact()
+	{
+		System.out.println("contact created");
+	}
+	@Test
+	public void modifiedcontact()
+	{
+		System.out.println("contact modified");
+	}
+	@Test
+	public void deletecontact()
+	{
+		System.out.println("cotact deleted");
+	}
+
+}
---
+++ src/test/java/dDT/OrganizationTest.java
@@ -0,0 +1,22 @@
+package dDT;
+
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+	@Test
+	public void createOrganization()
+	{
+		System.out.println("organization created");
+	}
+	@Test
+	public void modifyorganization()
+	{
+		System.out.println("organization modified");
+	}
+	@Test
+	public void deleteorganization()
+	{
+		System.out.println("organization deleted");
+	}
+
+}
